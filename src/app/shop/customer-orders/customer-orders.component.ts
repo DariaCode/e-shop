22acf83af11afaf2b27aaf59344f7553b9b9c1ff
DariaCode <54681621+DariaCode@users.noreply.github.com>
2023-09-04 18:08:25 +0200

    dv-34-1

--- src/app/shop/customer-orders/customer-orders.component.ts
+++ src/app/shop/customer-orders/customer-orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import { OrderService } from '../../shared/services/order.service';
 import { switchMap } from 'rxjs/operators';
